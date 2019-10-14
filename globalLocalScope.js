16abc375ba804f56c6b0a126bcb454fd73db055a
penseurtank <ravi.rsinghvaishali@gmail.com>
2019-10-14 17:18:26 +0530

    local & global scope

---
+++ globalLocalScope.js
@@ -0,0 +1,15 @@
+function a() {
+    let name = 'Ravi';
+    b();
+    console.log(name);
+}
+function b(){
+    let name ='Ranjan';
+    c();
+    console.log(name);
+}
+function c(){
+    let name = 'Singh';
+    console.log(name);
+}
+a();
